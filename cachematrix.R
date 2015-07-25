def6c9589b07561b3ba178a2b3edc063ab0fc39e
Mauro Batistic <mauro.batistic@gmail.com>
2015-07-25 17:39:45 -0300

    computes inverse and caches Matrix
    Two R functions were created to compute a matrix inverse and cache it,
    in order to save CPU and program run time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two R functions were created to compute a matrix inverse and cache it, in order 
+## to save CPU and program run time:
+## 1- makeCacheMatrix creates a special "matrix" object that can cache its inverse
+## 2- cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL
+    setM <- function(y) { ## setM creates a matrix in the cache environment
+        x <<- y
+        m <<- NULL
+    }
+    getM <- function() x  ## getM retrieves a matrix from the cache environment
+    setMinv <- function(inv) m <<- inv  ## setMinv puts an inverse matrix in the cache
+    getMinv <- function() m             ## getMinv gets the inverse matrix from the cache
+    list(setM = setM, getM = getM,      ## creates a list with the new function names
+         setMinv = setMinv, getMinv = getMinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix
+## If the inverse has already been calculated (and wasn't changed) the cached inverse 
+## matrix is returned, in order to save CPU time
+  
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+    m <- x$getMinv()
+    if(!is.null(m)) { ## if the 'x' inverse matrix is found in cache, return it!
+        message("getting cached data")
+        return(m) 
+    }
+    dados <- x$getM() ## otherwise compute 'x' inverse matrix
+    m <- solve(dados)
+    x$setMinv(m)      ## put it in the cache
+    m                 ## and return 'x' inverse matrix
+
 }
